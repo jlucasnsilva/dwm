ea1e304151a818ea988ba0ee00fbe09f8558be83
João Lucas <jlucas.nsilva@gmail.com>
2020-12-19 22:49:22 -0300

    Fixing the tabbed command

--- config.h
+++ config.h
@@ -25,13 +25,13 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5" };
 
-static const Rule rules[10] = {
+static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	/* { "Gimp",     NULL,       NULL,       0,            1,           -1 }, */
+	{ "Gimp",     NULL,       NULL,       1 << 1,            1,           -1 },
 	/* { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 }, */
 };
 
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "tabbed -c -r 2 st -w \"\"", NULL };
+static const char *termcmd[]  = { "tabbed", "-c", "-r", "2", "st", "-w", "\"\"", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
