a82e54074b16f1364047a426c4c4c2b66c07ea78
João Lucas <jlucas.nsilva@gmail.com>
2020-12-19 22:36:17 -0300

    up

--- config.h
+++ config.h
@@ -25,13 +25,13 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5" };
 
-static const Rule rules[] = {
+static const Rule rules[10] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       1 << 1,            1,           -1 },
+	/* { "Gimp",     NULL,       NULL,       0,            1,           -1 }, */
 	/* { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 }, */
 };
 
