19b0e0adb4ad93182540937186c5331b198a9e27
João Lucas <jlucas.nsilva@gmail.com>
2020-12-24 10:30:37 -0300

    Added audio control

--- config.def.h
+++ config.def.h
@@ -68,11 +68,21 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *firefoxcmd[]  = { "firefox", NULL };
+static const char *bravecmd[]  = { "brave-browser", NULL };
+static const char *volumeup[]   = { "amixer", "set", "'Master'", "10%+",     NULL };
+static const char *volumedown[] = { "amixer", "set", "'Master'", "10%-",     NULL };
+static const char *volumemute[] = { "amixer", "-D", "pulse", "set", "Master", "1+", "toggle" };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+    { 0,              XF86XK_AudioLowerVolume, spawn,          {.v = volumedown } },
+	{ 0,                     XF86XK_AudioMute, spawn,          {.v = volumemute } },
+	{ 0,              XF86XK_AudioRaiseVolume, spawn,          {.v = volumeup   } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_z,      spawn,          {.v = bravecmd } },
+	{ MODKEY,                       XK_x,      spawn,          {.v = firefoxcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
